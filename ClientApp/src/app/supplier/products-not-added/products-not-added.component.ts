f0367f20d2ccf3cf523d151a3ead502f9467d3bf
Shine <nguyenphucan2502@gmail.com>
2019-04-10 16:32:02 +0700

    i

--- ClientApp/src/app/supplier/products-not-added/products-not-added.component.ts
+++ ClientApp/src/app/supplier/products-not-added/products-not-added.component.ts
@@ -19,7 +19,7 @@ interface ProductsNotAdded {
   styleUrls: ['./products-not-added.component.css']
 })
 export class ProductsNotAddedComponent implements OnInit {
-  productsNotAdded?: ProductsNotAdded;
+  dataSource?: ProductsNotAdded[];
   supplierId = +this.route.snapshot.params.supplierId;
 
   @Input() title: string;
@@ -38,34 +38,43 @@ export class ProductsNotAddedComponent implements OnInit {
 
   private getProductsNotAdded(supplierId: number) {
     if (supplierId > 0) {
-      this.supplierService.getProductsNotAdded(supplierId).subscribe((res: ProductsNotAdded) => {
-        this.productsNotAdded = res;
+      this.supplierService.getProductsNotAdded(supplierId).subscribe((res: ProductsNotAdded[]) => {
+        this.dataSource = res;
       });
     }
   }
 
-  onAdd(productId: number) {
+  onAdd(product: ProductBuy) {
     const entity = <SupplierProduct>{
       personId: this.supplierId,
-      productId: productId
+      productId: product.productId
     };
 
     const dialogRef = this.confirmService.openDialog(`Are you sure to add this product?`);
 
-    // TODO remove newly add product
     dialogRef.afterClosed().subscribe(dialogRes => {
       if (dialogRes) {
-        this.supplierService.addSupplierProduct(entity).subscribe((res: SupplierProduct) => {
-          if (res) {
-            this.snackBar.open(`Product Added`, 'Succes');
-            // Find index of newly add product
-            const index = this.productsNotAdded.products.findIndex(p => p.productId === res.productId);
+        this.supplierService.addSupplierProduct(entity).subscribe(() => {
+          this.snackBar.open(`${product.productName} added`, 'Success');
 
-            // Remove newly add product from products list
-            if (index > -1) {
-              this.productsNotAdded.products.splice(index, 1);
+          // Array.some break when return true
+          this.dataSource.some(pna => {
+            const proIndex = pna.products.findIndex(p => p.productId === product.productId);
+
+            // Remove newly add product
+            if (proIndex !== -1) {
+              pna.products.splice(proIndex, 1);
+
+              // Remove category without any products
+              if (pna.products.length === 0) {
+                const catIndex = this.dataSource.findIndex(pn => pn.category === pna.category);
+
+                this.dataSource.splice(catIndex, 1);
+              }
+
+              return true;
             }
-          }
+          });
         });
       }
     });
